db6abe51286ead42c49a0b73dc8ccec22f5bed6c
Elizabeth-grider <148920090+Elizabeth-grider@users.noreply.github.com>
2024-09-04 19:41:54 +0000

    submit?

--- InsSortInt.java
+++ InsSortInt.java
@@ -1,3 +1,27 @@
-public class InsSortInt {
+import java.util.Scanner; 
+import java.io.File; 
+import java.io.FileNotFoundExpetion; 
 
+
+public class InsSortInt {
+ public static void main(String[] arr){
+    int[] arr = new int[1000]; 
+    try {
+        File file = new file("randomInt.txt"); 
+        Scanner Read
+    }
+
+
+
+
+ }
+
+ public class Sort(){
+    int checker = arr[1]; 
+    for(int i =0; i<arr.length; i++){
+        if(checker<arr[i]){
+            arr
+        }
+    }
+ }
 }
