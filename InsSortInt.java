e4c4bb3bf83606feee2d42950fcea3a9069076f0
Elizabeth-grider <148920090+Elizabeth-grider@users.noreply.github.com>
2024-09-09 00:37:04 +0000

    commit?

--- InsSortInt.java
+++ InsSortInt.java
@@ -1,27 +1,47 @@
 import java.util.Scanner; 
 import java.io.File; 
-import java.io.FileNotFoundExpetion; 
+import java.io.FileNotFoundException; 
 
 
 public class InsSortInt {
- public static void main(String[] arr){
-    int[] arr = new int[1000]; 
+ public static void main(String[] args){
+    int[] arrInt = new int[1000]; 
+    int i =0; 
+    String[] arrString = new String[1000]; 
     try {
-        File file = new file("randomInt.txt"); 
-        Scanner Read
+        File file = new File("randints.txt"); 
+        Scanner reader = new Scanner(file); 
+     while (reader.hasNextLine()){
+        String num= reader.nextLine(); 
+        i++; 
+    } 
     }
+ 
 
+    catch(FileNotFoundException e){
+        System.out.println("Error"); 
+        e.printStackTrace(); 
+    }
+    for(int j=0; j<1000; j++){
+        arrInt[j]= Integer.parseInt(arrString[j]); 
 
+    }
+    for(i=0; i<1000; i++){
+        int key =arrInt[i]; 
+        int j =i+1; 
+        while(j>=0&&arrInt[j]>key){
+            arrInt[j+1]= arrInt[j]; 
+            j=j-1; 
 
-
- }
-
- public class Sort(){
-    int checker = arr[1]; 
-    for(int i =0; i<arr.length; i++){
-        if(checker<arr[i]){
-            arr
         }
+        arrInt[j+1]=key; 
+
     }
+    System.out.println("smallest is "+arrInt[0] + "largest is " +arrInt[1000]); 
  }
 }
+
+
+
+
+ 
